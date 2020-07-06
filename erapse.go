b8339f5172548928fa2fd2dfb0d614188eaef877
Takeyuki UEDA <ueda@latelierdueda.com>
2020-07-06 14:06:55 +0900

    fix, exported

--- erapse.go
+++ erapse.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func showErapsedTIme(start time.Time) {
+func ShowErapsedTIme(start time.Time) {
 	pc, _ /*file*/, _ /*line*/, _ := runtime.Caller(1) // 1: caller function
 	f := runtime.FuncForPC(pc)                         // refrection for caller function
 	fmt.Printf("eraps %s: %d μs\n", f.Name(), time.Now().Sub(start).Microseconds())
