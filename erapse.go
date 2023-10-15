bd505a10962db52e71640a555b5d5df77b9b944a
Dr. Takeyuki Ueda <ueda@latelierdueda.com>
2023-10-15 19:51:34 +0900

    Update erapse.go
    use "log" instead of "fmt"

--- erapse.go
+++ erapse.go
@@ -1,7 +1,8 @@
 package erapse
 
 import (
-	"fmt"
+//	"fmt"
+	"log"
 	"runtime"
 	"time"
 )
@@ -9,5 +10,6 @@ import (
 func ShowErapsedTIme(start time.Time) {
 	pc, _ /*file*/, _ /*line*/, _ := runtime.Caller(1) // 1: caller function
 	f := runtime.FuncForPC(pc)                         // refrection for caller function
-	fmt.Printf("eraps %s: %d μs\n", f.Name(), time.Now().Sub(start).Microseconds())
+	//fmt.Printf("eraps %s: %d μs\n", f.Name(), time.Now().Sub(start).Microseconds())
+	log.Printf("eraps %s: %d μs\n", f.Name(), time.Now().Sub(start).Microseconds())
 }
